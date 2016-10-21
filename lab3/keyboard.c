4527008ec4369902d54af3b75e524823d5186931
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-10-21 09:01:11 +0000

    Fixed kbd_subscribe_int()

--- lab3/keyboard.c
+++ lab3/keyboard.c
@@ -1,1 +1,33 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <minix/com.h>
 #include "keyboard.h"
+
+int kbd_subscribe_int(int * hook_id)
+{
+	if ( sys_irqsetpolicy (KBC_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, hook_id) != OK ) {
+			printf("kbd_subscribe_int() -> FAILED sys_irqsetpolicy()\n");
+			return -1;
+		}
+
+	if ( sys_irqenable (hook_id) != OK ) {
+		printf("kbd_subscribe_int() -> FAILED sys_irqenable()\n");
+		return -1;
+	}
+
+	return *hook_id;
+}
+
+int kbd_unsubscribe_int(int * hook_id) {
+	if ( sys_irqdisable (hook_id) != OK ) {
+		printf("kbd_unsubscribe_int() -> FAILED sys_irqdisable()\n");
+		return -1;
+	}
+
+	if ( sys_irqrmpolicy (hook_id) != OK ) {
+		printf("kbd_unsubscribe_int() -> FAILED sys_irqrmpolicy()\n");
+		return -1;
+	}
+
+	return OK;
+}
--- lab3/keyboard.h
+++ lab3/keyboard.h
@@ -1,6 +1,8 @@
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
+
+
 /** @defgroup keyboard keyboard
  * @{
  *

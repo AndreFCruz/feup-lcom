c1e67b1a7fcba81373a3efd92bba120fe4e7fdba
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-10-21 08:52:38 +0000

    Added files to encapsulate keyboard generic functions

---
+++ lab3/keyboard.h
@@ -0,0 +1,23 @@
+#ifndef __KEYBOARD_H
+#define __KEYBOARD_H
+
+/** @defgroup keyboard keyboard
+ * @{
+ *
+ * Functions for using the i8042 KBC/KBD
+ */
+
+
+ /**
+  * @brief Subscribes and enables Keyboards interrupts
+  *
+  * @return Returns bit order in interrupt mask; negative value on failure
+  */
+ int kbd_subscribe_int(int * hook_id);
+
+ /**
+  * @brief Unsubscribes Keyboard interrupts
+  *
+  * @return Return 0 upon success and non-zero otherwise
+  */
+ int kbd_unsubscribe_int(int * hook_id);

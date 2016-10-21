bbb3e053b322f2e906ea36165c4c089119bc7798
André Cruz <AndreFCruz@users.noreply.github.com>
2016-10-21 11:14:15 +0000

    Completed keyboard_handler(), resolved include related conflicts

--- lab3/i8042.h
+++ lab3/i8042.h
@@ -7,28 +7,26 @@
  * Constants for programming the i8042 Keyboard Controller.
  */
 
-#define BIT(n) (0x01<<(n))
+//#define BIT(n) (0x01<<(n))
 
 #define KBC_IRQ             1   /**< @brief KBC IRQ line */
 #define KBD_INITIAL_HOOK_ID 1   /**< @brief KBD Initial hook_id */
 
 #define ESQ_BREAK_CODE      0x81    /**< @brief ESQ key break code */
 
-/* Timer 0 useful macros */
-#define TIMER0_IRQ	        0    /**< @brief Timer 0 IRQ line */
-#define TIMER0_IRQSET		0	 /**< @brief Timer 0 Policy Bit */
+// /* Timer 0 useful macros */
+// #define TIMER0_IRQ	        0    /**< @brief Timer 0 IRQ line */
+// #define TIMER0_IRQSET		0	 /**< @brief Timer 0 Policy Bit */
 
-/* I/O port addresses */
-
-#define TIMER_0			0x40 /**< @brief Timer 0 count register */
-#define TIMER_CTRL		0x43 /**< @brief Control register */
+// /* I/O port addresses */
+//
+// #define TIMER_0			0x40 /**< @brief Timer 0 count register */
+// #define TIMER_CTRL		0x43 /**< @brief Control register */
 
 // INPUT_BUFFER is accessed by ports 0x60 or 0x64, depending on context
 #define OUT_BUF         0x60 /**< @brief KBC Output buffer register */
 #define STAT_REG        0x64 /**< @brief KBC Status register */
 #define KBC_CMD_REG     0x64 /**< @brief KBC Command register */
-
-#define SPEAKER_CTRL	0x61 /**< @brief Register for speaker control  */
 
 /* KBC Status Register BITs' Meaning*/
 

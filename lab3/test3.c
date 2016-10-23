0021208a1d674c126b1602d63e34ed1ba50f0ee9
André Cruz <AndreFCruz@users.noreply.github.com>
2016-10-23 19:30:25 +0000

    Drafted kbd_timed_Scan(), missing timer interrupt logic

--- lab3/test3.c
+++ lab3/test3.c
@@ -17,12 +17,6 @@ int kbd_test_scan(unsigned short ass)
 		return 1;
 	}
 
-	// int timer_irq_set;
-	// if ( (timer_irq_set = BIT(timer_subscribe_int())) < 0 ) { // hook_id returned for Timer 0
-	// 	printf("kbd_test_scan() -> FAILED timer_subscribe_int()\n");
-	// 	return 1;
-	// }
-
 	int status = 0;	// keyboard status flag
 
 	int r;
@@ -59,19 +53,86 @@ int kbd_test_scan(unsigned short ass)
 		return 1;
 	}
 
+<<<<<<< .mine
+	printf("* keyboard_test_scan() Finished *\n");
+=======
 	// if ( timer_unsubscribe_int() < 0 ) {
 	// 	printf("kbd_test_scan() -> FAILED timer_unsubscribe_int()\n");
 	// 	return 1;
 	// }
 
 	printf("* keyboard_test_scan() Finished *\n");
+>>>>>>> .r50
 	return 0;
 }
+
 
 
 int kbd_test_leds(unsigned short n, unsigned short *leds) {
     /* To be completed */
 }
-int kbd_test_timed_scan(unsigned short n) {
-    /* To be completed */
+
+
+// ONLY A DRAFT . TODO: Finish interrupt logic
+int kbd_test_timed_scan(unsigned short n)
+{
+	int ipc_status;
+	message msg;
+
+	int keyboard_irq_set;
+	if ( (keyboard_irq_set = BIT(kbd_subscribe_int())) < 0 ) { // hook_id returned for keyboard
+		printf("kbd_test_scan() -> FAILED kbd_subscribe_int()\n");
+		return 1;
+	}
+	 int timer_irq_set;
+	 if ( (timer_irq_set = BIT(timer_subscribe_int())) < 0 || timer_set_square(0, 60) != OK ) { // hook_id returned for Timer 0
+	 	printf("kbd_test_scan() -> FAILED timer_subscribe_int()\n");
+	 	return 1;
+	 }
+
+	int status = 0;	// keyboard status flag
+
+	int r;
+	while( status != 2 ) { // While ESC BreakCode not detected
+		/* Get a request message. */
+		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+			printf("driver_receive failed with: %d\n", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE: /* hardware interrupt notification */
+					if (msg.NOTIFY_ARG & keyboard_irq_set) { /* keyboard interrupt */
+						if ( keyboard_handler(&status) != OK ) {
+							printf("kbd_test_scan() -> FAILED keyboard_handler()\n");
+							// return 1;
+						}
+					}
+					if (msg.NOTIFY_ARG & timer_irq_set) { /* timer interrupt */
+//						if ( keyboard_handler(&status) != OK ) {
+//							printf("kbd_test_scan() -> FAILED keyboard_handler()\n");
+//							// return 1;
+//						}
+					}
+					break;
+				default:
+					break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+			printf("Error: received unexpected standard message.\n");
+		}
+	}
+
+	if ( kbd_unsubscribe_int() < 0 ) {
+		printf("kbd_test_scan() -> FAILED kbd_unsubscribe_int()\n");
+		return 1;
+	}
+	 if ( timer_unsubscribe_int() < 0 ) {
+	 	printf("kbd_test_scan() -> FAILED timer_unsubscribe_int()\n");
+	 	return 1;
+	 }
+
+	printf("\n* keyboard_test_scan() Finished *\n");
+	return 0;
 }

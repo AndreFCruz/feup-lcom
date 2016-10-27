4997511f844cecd8a420546c39c7b66de23e99fa
up201503784 <up201503784@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-10-27 23:05:12 +0000

    Completed kbd_togle_leds(). Added comments

--- lab3/test3.c
+++ lab3/test3.c
@@ -113,9 +113,10 @@ int kbd_test_leds(unsigned short n, unsigned short *leds)
 	int status = 0;	// keyboard status flag
 	unsigned short idx = 0;	// Index in leds array
 	unsigned timerCount = 0;
+	unsigned long led_status = 0; //Initialize all LEDs as 0
 
 	int r;
-	while( idx < n ) { // While ESC BreakCode not detected
+	while( idx < n ) { // While Index not equal to array number of elements
 		/* Get a request message. */
 		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
 			printf("driver_receive failed with: %d\n", r);
@@ -128,8 +129,8 @@ int kbd_test_leds(unsigned short n, unsigned short *leds)
 						// Do nothing TODO: check
 					}
 					if (msg.NOTIFY_ARG & timer_irq_set) { /* timer interrupt */
-						if ( timerCount++ % 60 == 0 )
-							if ( keyboard_toggle_led(leds[idx++]) != OK ) {
+						if ( timerCount++ % 60 == 0 )	//Assuring 1 sec between led toggles
+							if ( keyboard_toggle_led(leds[idx++], & led_status) != OK ) {
 								printf("kdt_test_leds() -> FAILED keyboard_toggle_led()\n");
 								return 1;
 							}
@@ -145,11 +146,11 @@ int kbd_test_leds(unsigned short n, unsigned short *leds)
 	}
 
 	if ( kbd_unsubscribe_int() < 0 ) {
-		printf("kbd_test_scan() -> FAILED kbd_unsubscribe_int()\n");
+		printf("kbd_test_leds() -> FAILED kbd_unsubscribe_int()\n");
 		return 1;
 	}
 	 if ( timer_unsubscribe_int() < 0 ) {
-	 	printf("kbd_test_scan() -> FAILED timer_unsubscribe_int()\n");
+	 	printf("kbd_test_leds() -> FAILED timer_unsubscribe_int()\n");
 	 	return 1;
 	 }
 
@@ -166,21 +167,21 @@ int kbd_test_timed_scan(unsigned short n)
 
 	int keyboard_irq_set;
 	if ( (keyboard_irq_set = BIT(kbd_subscribe_int())) < 0 ) { // hook_id returned for keyboard
-		printf("kbd_test_scan() -> FAILED kbd_subscribe_int()\n");
+		printf("kbd_test_timed_scan() -> FAILED kbd_subscribe_int()\n");
 		return 1;
 	}
 	 int timer_irq_set;
 	 if ( (timer_irq_set = BIT(timer_subscribe_int())) < 0 || timer_set_square(0, 60) != OK ) { // hook_id returned for Timer 0
-	 	printf("kbd_test_scan() -> FAILED timer_subscribe_int()\n");
+	 	printf("kbd_test_timed_scan() -> FAILED timer_subscribe_int()\n");
 	 	return 1;
 	 }
 
 	int status = 0;	// keyboard status flag
-	unsigned short seconds = 0;
+	unsigned short seconds = 0; //Counter of seconds elapsed
 	unsigned timerCount = 0;
 
 	int r;
-	while( status != 2 && seconds < n ) { // While ESC BreakCode not detected
+	while( status != 2 && seconds < n ) { // While ESC BreakCode not detected or number of seconds elapsed not equal to n
 		/* Get a request message. */
 		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
 			printf("driver_receive failed with: %d\n", r);
@@ -191,9 +192,10 @@ int kbd_test_timed_scan(unsigned short n)
 				case HARDWARE: /* hardware interrupt notification */
 					if (msg.NOTIFY_ARG & keyboard_irq_set) { /* keyboard interrupt */
 						if ( keyboard_handler(&status) != OK ) {
-							printf("kbd_test_scan() -> FAILED keyboard_handler()\n");
+							printf("kbd_test_timed_scan() -> FAILED keyboard_handler()\n");
 							return 1;
 						}
+						//Restarting the seconds counting
 						timerCount = 0;
 						seconds = 0;
 					}
@@ -212,15 +214,15 @@ int kbd_test_timed_scan(unsigned short n)
 	}
 
 	if ( kbd_unsubscribe_int() < 0 ) {
-		printf("kbd_test_scan() -> FAILED kbd_unsubscribe_int()\n");
+		printf("kbd_test_timed_scan() -> FAILED kbd_unsubscribe_int()\n");
 		return 1;
 	}
 	 if ( timer_unsubscribe_int() < 0 ) {
-	 	printf("kbd_test_scan() -> FAILED timer_unsubscribe_int()\n");
+	 	printf("kbd_test_timed_scan() -> FAILED timer_unsubscribe_int()\n");
 	 	return 1;
 	 }
 
-	printf("\n* keyboard_test_scan() Finished *\n");
+	printf("\n* keyboard_test_timed_scan() Finished *\n\n");
 	return 0;
 }
 

ddd213718e12c1aacd325dafb3a429c055cdce1d
up201503784 <up201503784@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-11-02 01:47:56 +0000

    Started to write generic function mouse_write_command

--- lab4/mouse.c
+++ lab4/mouse.c
@@ -38,3 +38,20 @@ int mouse_unsubscribe_int(void)
         return MOUSE_INITIAL_HOOK_ID;
 }
 
+
+//TODO: funcao generica mouse_write_command que escreve um comando no mouse. Serve para funcoes 1,2 e 3! Usa kbd_read e kbd_write!
+
+//Draft
+int mouse_write_command()
+{
+
+	if (keyboard_write (WRITE_B_MOUSE) != OK)
+		return 1;		//Print of error is done in keyboard_write()
+
+	if ( (kbdResponse = keyboard_read ()) == -1 ) {
+		printf ("keyboard_write_command() -> FAILED sys_inb()\n");
+		return 1;
+	}
+
+	//Talvez nao de para usar as kbd_write e kbd_read. Pela necessidade de verificar o bit 5.... VER isto
+}

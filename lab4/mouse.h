8cb69103128591514eb510f18bf82c7da1a25858
up201503784 <up201503784@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-11-03 20:06:33 +0000

    Completed mouse_write and mouse_read. Working on test_packet

--- lab4/mouse.h
+++ lab4/mouse.h
@@ -23,3 +23,41 @@
   * @return Return 0 upon success and non-zero otherwise
   */
  int mouse_unsubscribe_int(void);
+
+ /**
+  * @brief Reads data from the mouse output buffer
+  *
+  * @return Return value read upon sucess and -1 otherwise
+ */
+ int mouse_read(void);
+
+ /**
+  * @brief Writes data to the mouse input buffer
+  *
+  * @param parameter that is written in the input buffer
+  *
+  * @return Return 0 upon success and non-zero otherwise
+  */
+ int mouse_write(char data);
+
+ /**
+  * @brief Prints the packet in a human friendly way
+  *
+  * @param pointer to the array that contains the bytes from the packet
+  * @param number of elements of the packet
+  *
+  * @return Return 0 upon success and non-zero otherwise
+  */
+ int print_packet (unsigned char * packet, int nElements);
+
+ //TODO: REVIEW THE DOCUMENTATION
+ /**
+  * @brief Mouse Interrupt Handler
+  *
+  * Fetches Mouse output and prints packets in a human friendly way
+  *
+  * @param pointer to previous iteration's status
+  *
+  * @return Return 0 upon success and non-zero otherwise
+  */
+ int mouse_handler();

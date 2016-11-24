006cef3a5c570fea6c19ae8fed4ec533ab8d401a
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-11-24 14:50:54 +0000

    lab5.c final

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <errno.h>
 #include "test5.h"
+#include "pixmap.h"
 
 static int proc_args(int argc, char **argv);
 static unsigned long parse_ulong(char *str, int base);
@@ -19,7 +20,7 @@ int main(int argc, char **argv)
 	else return proc_args(argc, argv);
 }
 
-static void print_usage(char **argv)	//TODO: Re-Verificar se as chamadas são assim - principalmente a xpm
+static void print_usage(char **argv)
 {
 	printf("Usage: one of the following:\n"
 			"\t service run %s -args \"init <decimal no.- mode> <decimal no.- delay>\"\n"
@@ -38,131 +39,149 @@ static int proc_args(int argc, char **argv)
 
 	if (strncmp(argv[1], "init", strlen("init")) == 0) {
 		if (argc != 4) {
-			printf("video card: wrong no. of arguments for test_init()\n");
+			printf("lab5: wrong no. of arguments for test_init()\n");
 			return 1;
 		}
-		mode = parse_ulong(argv[2], 16);						/* Parses string to unsigned long, in hex */
-		if (mode > SHRT_MAX)
+		mode = parse_ulong(argv[2], 16);
+		if (mode > USHRT_MAX) {
+			printf("lab5: mode exceeds USHRT_MAX, was %u", mode);
 			return 1;
+		}
 
-		time = parse_ulong(argv[3], 10);						/* Parses string to unsigned long, in decimal */
-		if (time > SHRT_MAX)
+		time = parse_ulong(argv[3], 10);
+		if (time > USHRT_MAX) {
+			printf("lab5: time exceeds USHRT_MAX, was %u", time);
 			return 1;
+		}
 
-		printf("video card::test_init(%X, %lu)\n", mode, time);
+		printf("lab5::test_init(%X, %lu)\n", mode, time);
 		if ( test_init(mode, time) == NULL )
 			printf("TestInit Returned NULL.\n");
 	}
 	else if (strncmp(argv[1], "square", strlen("square")) == 0) {
 		if (argc != 6) {
-			printf("video card: wrong no. of arguments for test_square()\n");
+			printf("lab5: wrong no. of arguments for test_square()\n");
 			return 1;
 		}
-		xbegin = parse_ulong(argv[2], 10);						/* Parsing all the arguments */
-		if (xbegin > SHRT_MAX)
+
+		if ( (xbegin = parse_ulong(argv[2], 10)) > USHRT_MAX )
 			return 1;
 
-		ybegin = parse_ulong(argv[3], 10);
-		if (ybegin > SHRT_MAX)
+		if ( (ybegin = parse_ulong(argv[3], 10)) > USHRT_MAX )
 			return 1;
 
-		size = parse_ulong(argv[4], 10);
-		if (size > SHRT_MAX)
+		if ( (size = parse_ulong(argv[4], 10)) > USHRT_MAX )
 			return 1;
 
-		color = parse_ulong(argv[5], 10);
-		if (color == ULONG_MAX)
+		if ( (color = parse_ulong(argv[5], 10)) == ULONG_MAX )
 			return 1;
 
-		printf("video card::test_async(%lu, %lu, %lu, %lu)\n", xbegin, ybegin, size, color);
+		printf("lab5::test_async(%lu, %lu, %lu, %lu)\n", xbegin, ybegin, size, color);
 		return test_square(xbegin, ybegin, size, color);
 	}
 	else if (strncmp(argv[1], "line", strlen("line")) == 0) {
 		if (argc != 7) {
-			printf("video card: wrong no of arguments for test_line()\n");
+			printf("lab5: wrong no of arguments for test_line()\n");
 			return 1;
 		}
-		xbegin = parse_ulong(argv[2], 10);						/* Parsing all the arguments */
-		if (xbegin > SHRT_MAX)
+		if ( (xbegin = parse_ulong(argv[2], 10)) > USHRT_MAX )
 			return 1;
 
-		ybegin = parse_ulong(argv[3], 10);
-		if (ybegin > SHRT_MAX)
+		if ( (ybegin = parse_ulong(argv[3], 10)) > USHRT_MAX )
 			return 1;
 
-		xend = parse_ulong(argv[4], 10);
-		if (xend > SHRT_MAX)
+		if ( (xend = parse_ulong(argv[4], 10)) > USHRT_MAX )
 			return 1;
 
-		yend = parse_ulong(argv[5], 10);
-		if (yend > SHRT_MAX)
+		if ( (yend = parse_ulong(argv[5], 10)) > USHRT_MAX )
 			return 1;
 
-		color = parse_ulong(argv[6], 10);
-		if (color == ULONG_MAX)
+		if ( (color = parse_ulong(argv[6], 10)) == ULONG_MAX )
 			return 1;
 
-		printf("video card::test_config(%lu, %lu, %lu, %lu, %lu)\n", xbegin, ybegin, xend, yend, color);
+		printf("lab5::test_config(%lu, %lu, %lu, %lu, %lu)\n", xbegin, ybegin, xend, yend, color);
 		return test_line(xbegin, ybegin, xend, yend, color);
 	}
 	else if (strncmp(argv[1], "xpm", strlen("xpm")) == 0) {
 		if (argc != 5) {
-			printf("video card: wrong no of arguments for test_xpm()\n");
+			printf("lab5: wrong no of arguments for test_xpm()\n");
 			return 1;
 		}
-		xbegin = parse_ulong(argv[2], 10);						/* Parsing all the arguments */
-		if (xbegin > SHRT_MAX)
+
+		if ( (xbegin = parse_ulong(argv[2], 10)) > USHRT_MAX)
 			return 1;
 
-		ybegin = parse_ulong(argv[3], 10);
-		if (ybegin > SHRT_MAX)
+		if ( (ybegin = parse_ulong(argv[3], 10)) > USHRT_MAX)
 			return 1;
 
-		//TODO:Falta aqui o controlo do pointer, perguntar amanha ao prof
-		printf("video card::test_xpm(%lu, %lu)\n", xbegin, ybegin ,"char*"); //PLACEHOLDER CHAR*
-//		return test_xpm(xbegin, ybegin, ...); // TODO CRAPPY PROGRAMMING
+		char * image[];
+		// Comparar argc[4] com o nome das varias imagens
+		if ( strncmp(argv[4], "pic1", strlen("pic1")) == 0 ) {
+			image = pic1;
+		} else if ( strncmp(argv[4], "pic2", strlen("pic2")) == 0 ) {
+			image = pic2;
+		} else if ( strncmp(argv[4], "cross", strlen("cross")) == 0 ) {
+			image = cross;
+		} else if ( strncmp(argv[4], "pic3", strlen("pic3")) == 0 ) {
+			image = pic3;
+		} else if ( strncmp(argv[4], "penguin", strlen("penguin")) == 0 ) {
+			image = penguin;
+		} else {
+			printf("lab5: there's no such image.\n");
+			return 1;
+		}
+
+		printf("lab5::test_xpm(%lu, %lu, %s)\n", xbegin, ybegin , argv[4]);
+		return test_xpm(xbegin, ybegin, image);
 	}
 	else if (strncmp(argv[1], "move", strlen("move")) == 0) {
 		if (argc != 8) {
-			printf("video card: wrong no of arguments for test_move()\n");
+			printf("lab5: wrong no of arguments for test_move()\n");
 			return 1;
 		}
-		xbegin = parse_ulong(argv[2], 10);						/* Parsing all the arguments */
-		if (xbegin > SHRT_MAX)
+
+		if ( (xbegin = parse_ulong(argv[2], 10)) > USHRT_MAX)
+			return 1;
+		if ( (ybegin = parse_ulong(argv[3], 10)) > USHRT_MAX)
+			return 1;
+		if ( (hor = parse_ulong(argv[5], 10)) > USHRT_MAX)
+			return 1;
+		if ( (delta = parse_long(argv[6], 10)) > SHRT_MAX || delta < SHRT_MIN)
+			return 1;
+		if ( (time = parse_ulong(argv[7], 10)) > USHRT_MAX)
 			return 1;
 
-		ybegin = parse_ulong(argv[3], 10);
-		if (ybegin > SHRT_MAX)
+		char * image[];
+		// Comparar argc[4] com o nome das varias imagens
+		if ( strncmp(argv[4], "pic1", strlen("pic1")) == 0 ) {
+			image = pic1;
+		} else if ( strncmp(argv[4], "pic2", strlen("pic2")) == 0 ) {
+			image = pic2;
+		} else if ( strncmp(argv[4], "cross", strlen("cross")) == 0 ) {
+			image = cross;
+		} else if ( strncmp(argv[4], "pic3", strlen("pic3")) == 0 ) {
+			image = pic3;
+		} else if ( strncmp(argv[4], "penguin", strlen("penguin")) == 0 ) {
+			image = penguin;
+		} else {
+			printf("lab5: there's no such image.\n");
 			return 1;
+		}
 
-		//TODO: controlo do xpm aqui tb
-
-		hor = parse_ulong(argv[5], 10);
-		if (hor > SHRT_MAX)
-			return 1;
-
-		delta = parse_long(argv[6], 10);
-		if (delta > SHRT_MAX || delta < SHRT_MIN)
-			return 1;
-
-		time = parse_ulong(argv[7], 10);
-		if (time > SHRT_MAX)
-			return 1;
-
-		printf("video card::test_move(%d)\n", xbegin, ybegin, "char* PlaceHolder", hor, delta, time); //PLACEHOLDER CHAR*
-//		return test_move(xbegin, ybegin, ..., hor, delta, time);	// TODO
+		printf("lab5::test_move(%lu, %lu, %s, %lu, %lu, %lu)\n", xbegin, ybegin, argv[4], hor, delta, time);
+		return test_move(xbegin, ybegin, image, hor, delta, time);
 	}
 	else if (strncmp(argv[1], "controller", strlen("controller")) == 0) {
 		if (argc != 2) {
-			printf("video card: wrong no of arguments for test_controller()\n");
+			printf("lab5: wrong no of arguments for test_controller()\n");
 			return 1;
 		}
 
-		printf("video card::test_controller()\n");
+		printf("lab5::test_controller()\n");
 		return test_controller();
 	}
 	else {
-		printf("video card: %s - no valid function!\n", argv[1]);
+		printf("lab5: %s - no valid function!\n", argv[1]);
 		return 1;
 	}
 }
@@ -182,7 +201,7 @@ static unsigned long parse_ulong(char *str, int base)
 	}
 
 	if (endptr == str) {
-		printf("video card: parse_ulong: no digits were found in %s\n", str);
+		printf("lab5: parse_ulong: no digits were found in %s\n", str);
 		return ULONG_MAX;
 	}
 
@@ -209,7 +228,7 @@ static signed long parse_long(char *str, int base)
 	}
 
 	if (endptr == str) {
-		printf("video card: parse_long: no digits were found in %s\n", str);
+		printf("lab5: parse_long: no digits were found in %s\n", str);
 		return LONG_MAX;	//Could also be LONG_MIN
 	}
 

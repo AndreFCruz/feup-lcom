cd2730947c6adb6dfa9db1b79a2a72e295b2afc0
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-11-30 16:53:03 +0000

    preliminary Input struct

---
+++ proj/src/Input.c
@@ -0,0 +1,5 @@
+#include "Input.h"
+
+//input_t * new_input() {
+//
+//}
---
+++ proj/src/Input.h
@@ -0,0 +1,25 @@
+#ifndef __INPUT_H
+#define __INPUT_H
+
+typedef enum {MAKE, BREAK /* pressed/released? */} key_state;
+
+typedef enum {UP, DOWN /* pressed/released? */} button_state;
+
+typedef struct {
+	/* Keyboard Keys */
+	key_state ESC;
+	key_state ENTER;
+
+	/* Mouse Buttons */
+	button_state RMB;
+	button_state LMB;
+	button_state MMB;
+
+	/* Mouse Position */
+	unsigned mouse_pos[2];
+} input_t;
+
+input_t * new_input();	// Constructor
+void delete_input(input_t * input);	// Destructor
+
+#endif

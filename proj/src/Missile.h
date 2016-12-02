8ae811d8ff09c6ba6ebadc51b8c0b7eaaf23247c
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-12-02 12:21:45 +0000

    Saving Class Changes

--- proj/src/Missile.h
+++ proj/src/Missile.h
@@ -1,12 +1,24 @@
 #ifndef __MISSILE_H
 #define __MISSILE_H
 
-struct Missile;
-typedef struct Missile missile_t;
+struct missile_t;
+typedef struct missile_t Missile;
+
+struct explosion_t;
+typedef struct explosion_t Explosion;
+
+/* Missile's Methods */
+Missile * new_emissile(unsigned * init_pos, float * vel);
+Missile * new_fmissile(unsigned * init_pos, float * vel, unsigned * mouse_pos);
 
 unsigned missile_getXPos(Missile * ptr);
 unsigned missile_getYPos(Missile * ptr);
 void missile_update(Missile * ptr);
+
+Explosion * delete_missile(Missile * ptr);
+
+/* Explosion's Methods */
+
 
 
 #endif

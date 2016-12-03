5a6c035b3dffadf2e5896463b4ba70db66e1f9f7
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-12-03 08:55:33 +0000

    Generic Vector struct added, incomplete

---
+++ proj/src/GVector.h
@@ -0,0 +1,22 @@
+#ifndef __GVECTOR_H
+#define __GVECTOR_H
+
+/**
+ * Struct for Generic Vector Container
+ */
+
+struct gvector_t;
+typedef struct gvector_t GVector;
+
+/* Methods */
+GVector * new_gvector(unsigned size, unsigned el_size);
+void delete_gvector(GVector * self);
+
+void * gvector_at(GVector * self, unsigned index);
+void * gvector_push_back(GVector * self, void * elem);  // realloc may happen
+
+void gvector_pop_back(GVector * self);                  // realloc may happen
+void gvector_clear(GVector * self);
+
+
+#endif

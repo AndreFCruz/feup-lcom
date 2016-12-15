98987daaf3cc47e5b07f6ef0ed9edb5b83897d2b
up201503776 <up201503776@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2016-12-15 20:09:14 +0000

    saving evening's work

--- proj/src/Missile.h
+++ proj/src/Missile.h
@@ -12,13 +12,13 @@ typedef struct explosion_t Explosion;
 
 /* Missile's Methods */
 Missile * new_emissile();
-Missile * new_fmissile(const unsigned * init_pos, const unsigned * mouse_pos);
+Missile * new_fmissile(const int * init_pos, const int * mouse_pos);
 
 size_t missile_getSizeOf();
-unsigned missile_getPosX(Missile * ptr);
-unsigned missile_getPosY(Missile * ptr);
-unsigned missile_getInitX(Missile * ptr);
-unsigned missile_getInitY(Missile * ptr);
+int missile_getPosX(Missile * ptr);
+int missile_getPosY(Missile * ptr);
+int missile_getInitX(Missile * ptr);
+int missile_getInitY(Missile * ptr);
 uint16_t missile_getColor(Missile * ptr);
 int missile_update(Missile * ptr);
 int missile_isFriendly(Missile * ptr);
@@ -28,14 +28,14 @@ Explosion * delete_missile(Missile * ptr);
 int missile_atExplosion(Missile * ptr, Explosion * e_ptr);	// TODO MOVE THIS FUNCTION ELSEWHERE
 
 /* Explosion's Methods */
-Explosion * new_explosion(const unsigned * position);
+Explosion * new_explosion(const int * position);
 
 int explosion_update(Explosion * ptr);
 
 Bitmap * explosion_getBitmap(Explosion * ptr);
 
-unsigned explosion_getPosX(Explosion * ptr);
-unsigned explosion_getPosY(Explosion * ptr);
+int explosion_getPosX(Explosion * ptr);
+int explosion_getPosY(Explosion * ptr);
 
 size_t explosion_getSizeOf();
 

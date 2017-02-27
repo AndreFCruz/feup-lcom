dd297a379f4046facee7c6170ecd69538e7f2d53
up201503784 <up201503784@d60ffdf3-4cf9-45b7-847d-7e28926f93d5>
2017-02-27 12:15:33 +0000

    Tests. Only printf's added.

--- proj/src/Communication.c
+++ proj/src/Communication.c
@@ -3,9 +3,12 @@
 
 // Current state of communication
 static serial_state_t comState = NONE;
+static int flag = 0;
 
 void serial_handler() {
 	unsigned char received = serial_read();
+
+	printf("MP_WAITING: %x\nMP_ONGOING: %x\nMP_ENDED: %x\nNONE: %x\n",(int)MP_WAITING,(int) MP_ONGOING,(int) MP_ENDED,(int) NONE);
 
 	printf("-SH- State: %x. Received: %x.\n", (int) comState, received);
 
@@ -38,18 +41,24 @@ void serial_handler() {
 		printf("** SH - received com while out of MP **\n");
 		break;
 	default:
-		printf("\n** SH - SHOULD NOT BE POSSIBLE **\n");
-	}
+			printf("\n** SH - SHOULD NOT BE POSSIBLE **\n");
+		}
 }
 
 void setComState(serial_state_t state) {
-	printf("SetComState called. State: %x.\n", (int) state);
+	printf("SetComState called. State: %x.\n", (char) state);
+
+	printf("MP_WAITING: %x\nMP_ONGOING: %x\nMP_ENDED: %x\nNONE: %x\n",(char)MP_WAITING,(char) MP_ONGOING,(char) MP_ENDED,(char) NONE);
 
 	// appropriately write to serial on state change
 	comState = state;
-	serial_write(state);
+	serial_write((char)state);
 }
 
 serial_state_t getComState() {
 	return comState;
+}
+
+int getflag() {
+	return flag;
 }
